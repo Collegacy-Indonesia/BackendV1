59f437fadcfa953fe9d302178ab299b12e1d70c6
MSMazaya <msulthanmazaya@gmail.com>
2021-10-19 11:20:44 +0700

    docs(readme): create step-by-step installation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#[macro_use]
 extern crate diesel;
 extern crate dotenv;
 
@@ -7,24 +6,20 @@ use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
 
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 mod echo;
 
 fn extablish_connection() -> MysqlConnection {
     dotenv().ok();
-    let database_url = env::var("DATABASE_URL")
-        .expect("Database url must be set in .env")
+    let database_url = env::var("DATABASE_URL").expect("Database url must be set in .env");
     MysqlConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}",database_url))
+        .expect(&format!("Error connecting to {}", database_url))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(echo::echo)
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(echo::echo))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
