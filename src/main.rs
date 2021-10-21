c08fbd5e437c8e09310481e1c5f6ffddfd918071
MSMazaya <msulthanmazaya@gmail.com>
2021-10-21 07:29:36 +0700

    feature(route): create nested route structure and implement for user

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 extern crate diesel;
 extern crate dotenv;
 
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
 use diesel::{
     r2d2::{self, ConnectionManager},
     MysqlConnection,
@@ -30,13 +30,14 @@ async fn main() -> std::io::Result<()> {
         .expect("Failed to create pool.");
 
     HttpServer::new(move || {
-        App::new()
-            .data(pool.clone())
-            .service(controllers::user::get_all_user)
-            .service(controllers::user::get_user_by_id)
-            .service(controllers::user::update_user)
-            .service(controllers::user::create_user)
-            .service(controllers::user::delete_user)
+        App::new().data(pool.clone()).service(
+            web::scope("/users")
+                .service(controllers::user::get_user_by_id)
+                .service(controllers::user::update_user)
+                .service(controllers::user::create_user)
+                .service(controllers::user::delete_user)
+                .service(controllers::user::get_all_user),
+        )
     })
     .bind("127.0.0.1:8080")?
     .run()
